e71ae4bb165c9855810f8afba7e8b0f4e54af832
ArTeGaS-2 <probablytorvik@gmail.com>
2025-09-21 11:50:04 +0300

    Фікси багів, наведення порядку в файлах проекту. Дописали Advanced Level Generator до першого стану

--- Assets/Advanced_Level_Generator.cs
+++ Assets/Advanced_Level_Generator.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class Advanced_Level_Generator : MonoBehaviour
 {
+    public static Advanced_Level_Generator instance;
+
     public GameObject levelPrefab; // Шаблон рівня
     public int levelNum;
     public float levelHeightStep = 14f; // Крок між поверхами
     public int levelsNumToGenerate = 10; // Кількість рівнів
 
+    public GameObject player;
+
+    public List<GameObject> levels = new List<GameObject>();
+
     private void Start()
     {
+        instance = this;
+
+        player = GameObject.FindWithTag("Player");
+
         for (int i = 0; i < levelsNumToGenerate + 1; i++)
         {
             levelNum--;
-            Instantiate(levelPrefab,
+            GameObject level = Instantiate(levelPrefab,
                 new Vector3(0, 7, 0) + new Vector3(
                     0, levelNum * levelHeightStep, 0),
                 Quaternion.identity);
+            if (levels.Count > 10)
+            {
+                level.SetActive(false);
+            }
+            levels.Add(level);
         }
     }
 }
