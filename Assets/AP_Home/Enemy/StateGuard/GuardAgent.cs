91c7bb776b10e14569e16151260117f761d16f68
ArTeGaS-2 <probablytorvik@gmail.com>
2026-04-11 21:33:37 +0300

    Продовжили роботу над паттерном стейт

--- Assets/AP_Home/Enemy/StateGuard/GuardAgent.cs
+++ Assets/AP_Home/Enemy/StateGuard/GuardAgent.cs
@@ -10,4 +10,73 @@ public class GuardAgent : MonoBehaviour
     private IGuardState currentState;
 
     [SerializeField] private string playerTag = "Player";
+    [SerializeField] private Transform[] points;
+
+    [SerializeField] private float idleRadius = 0.5f;
+    [SerializeField] private float idleMinTime = 25f;
+    [SerializeField] private float idleMaxTime = 35f;
+    [SerializeField] private float losePlayerTime = 10f;
+
+    [HideInInspector] public bool playerInTrigger;
+    [HideInInspector] public float idleTimer;
+    [HideInInspector] public float loseTimer;
+    [HideInInspector] public float idleMoveTimer;
+    [HideInInspector] public int pointIndex;
+    [HideInInspector] public Vector3 idleCenter;
+    [HideInInspector] public Vector3 idlePoint;
+
+    public NavMeshAgent Agent => agent;
+    public Transform Player => player;
+    public Transform[] Points => points;
+    public float IdleRadius => idleRadius;
+    public float IdleMinTime => idleMinTime;
+    public float IdleMaxTime => idleMaxTime;
+    public float LosePlayerTime => losePlayerTime;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        GameObject playerObj = GameObject.FindGameObjectWithTag(
+            playerTag);
+        if (playerObj != null) player = playerObj.transform;
+        else 
+        {
+            Debug.LogError($"Гравець з тегом '" +
+                $"{playerTag}' не знайдений!");
+        }
+        ChangeState(new GuardIdle());
+    }
+    private void Update()
+    {
+        if (currentState != null) currentState.Update(this);
+    }
+    public void ChangeState(IGuardState newState)
+    {
+        if (currentState != null) currentState.Exit(this);
+        currentState = newState;
+        currentState.Enter(this);
+    }
+    public bool ReachedPoint()
+    {
+        if (agent.pathPending) return false;
+        return agent.remainingDistance <= agent.stoppingDistance;
+    }
+    public Vector3 GetCurrentPoint()
+    {
+        if (points.Length == 0) return transform.position;
+        return points[pointIndex].position;
+    }
+    public void NextPoint()
+    {
+        if (points.Length == 0) return;
+        pointIndex++;
+        if (pointIndex >= points.Length) pointIndex = 0;
+    }
+    public void SetRandomIdlePoint()
+    {
+        Vector2 randomPos = Random.insideUnitCircle * idleRadius;
+        idlePoint = idleCenter + new Vector3(
+            randomPos.x, 0f, randomPos.y);
+    }
 }
--- Assets/AP_Home/Enemy/StateGuard/GuardIdle.cs
+++ Assets/AP_Home/Enemy/StateGuard/GuardIdle.cs
@@ -6,10 +6,34 @@ public class GuardIdle : IGuardState
 {
     public void Enter(GuardAgent guard)
     {
-        
+        guard.idleCenter = guard.transform.position;
+        guard.idleTimer = Random.Range(
+            guard.IdleMinTime,
+            guard.IdleMaxTime);
+        guard.idleMoveTimer = 0f;
+        guard.Agent.speed = 1.5f;
+        guard.SetRandomIdlePoint();
     }
     public void Update(GuardAgent guard)
     {
+        if (guard.playerInTrigger)
+        {
+            guard.ChangeState(new GuardChase());
+            return;
+        }
+        guard.idleTimer -= Time.deltaTime;
+        guard.idleMoveTimer -= Time.deltaTime;
+
+        if (guard.idleMoveTimer <= 0)
+        {
+            guard.SetRandomIdlePoint();
+            guard.idleMoveTimer = Random.Range(2f, 4f);
+        }
+        guard.Agent.SetDestination(guard.idleCenter);
+        if (guard.idleTimer <= 0)
+        {
+            guard.ChangeState(new GuardWalk());
+        }
 
     }
     public void Exit(GuardAgent guard)
